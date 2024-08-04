98c308cd34e5e226fa377a409c65e374b168a643
Valki11 <102394314+Valki11@users.noreply.github.com>
2024-08-03 23:39:57 -0600

    creacion

---
+++ src/calculadora/Calculadora.java
@@ -0,0 +1,19 @@
+
+package calculadora;
+
+/**
+ *
+ * @author keila
+ */
+public class Calculadora {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        Pantallacalculadora frn = new Pantallacalculadora();
+        frn.show();
+        
+    }
+    }
